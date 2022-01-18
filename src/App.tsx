b4fe9bf2d05d131bf22a2ecf34228abe11d1e348
EvgeniyHalimon <halimonevgeniy@gmail.com>
2022-01-19 00:07:59 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src="https://starserv.ru/uploads/monthly_2020_10/AnguishedUnselfishGodwit-max-1mb.gif.a110174b0718aaffe053dde243a331e8.gif" alt="gif" />
+    </div>
+  );
+}
+
+export default App;
