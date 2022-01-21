f91720c98b9a91aa49f1aae73f3387ce346d6109
EvgeniyHalimon <halimonevgeniy@gmail.com>
2022-01-21 20:05:17 +0200

    changes to slider section

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,6 +2,14 @@ import React from "react";
 
 export const Loading: React.FC = () => {
     return(
-        <img src="https://starserv.ru/uploads/monthly_2020_10/AnguishedUnselfishGodwit-max-1mb.gif.a110174b0718aaffe053dde243a331e8.gif" alt="gif" />
+        <img 
+            src="https://starserv.ru/uploads/monthly_2020_10/AnguishedUnselfishGodwit-max-1mb.gif.a110174b0718aaffe053dde243a331e8.gif" 
+            alt="gif" 
+            style={{
+                position: 'absolute',
+                left: '40%',
+                top: '40%',
+            }}    
+        />
     )
 }
--- src/components/SlideShow.tsx
+++ src/components/SlideShow.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
+import {Loading} from "./Loading"
 
 export const SlideShow: React.FC<any> = ({randomAnimes}) => {
     const [index, setIndex] = useState(0)
@@ -9,64 +10,66 @@ export const SlideShow: React.FC<any> = ({randomAnimes}) => {
         }
     }
 
-    console.log(randomAnimes, "<========= lkgdklgsglk;d")
+    const pages = randomAnimes ? randomAnimes.length : 3
 
-    /* useEffect(() => {
+    useEffect(() => {
         resetTimeout()
         timeoutRef.current = setTimeout(() => {
             setIndex((prevIndex) => 
-                prevIndex === randomAnimes.length - 1 ? 0 : prevIndex + 1
+                prevIndex === pages - 1 ? 0 : prevIndex + 1
             )
         }, 2500);
         return () => {
             resetTimeout()
         }
-    },[]) */
+    },[index])
 
     return(
+        randomAnimes ? 
         <div>
             <div className="slide-show">
-                <div 
-                    className="slide-show-slider"
-                    style={{transform: `translate3d(${-index * 100}%, 0, 0`}}
-                >
-                    {randomAnimes && randomAnimes.map((titles: any, index: number) => 
-                        <div 
+                    <div
+                        className="slide-show-slider"
+                        style={{ transform: `translate3d(${-index * 100}%, 0, 0` }}
+                    >
+                        {randomAnimes && randomAnimes.map((titles: any) => <div
                             className="slide"
                             key={titles.url}
                         >
-                            <div className="slide-item" key={titles.url}>
-                                <div>
-                                    <div className="info-wrapper">
-                                        <p>{titles.title} / {titles.title_japanese}</p> 
-                                        <p>{titles.aired.string}</p>
-                                        <p>Score : {titles.score}</p>
-                                        <p>Rank : {titles.rank}</p>
-                                        <div className="slide-genres">
-                                            {titles.genres.map((genre: any) =>
-                                                <p className="slide-name" key={genre.mal_id}>{genre.name}</p>
-                                            )}
+                            <div>
+                                <div className="slide-item" key={titles.url}>
+                                    <img className="slide-poster" src={titles.images.webp.image_url} alt="poster" />
+                                    <div>
+                                        <div>
+                                            <p>{titles.title} / {titles.title_japanese}</p>
+                                            <p>{titles.aired.string}</p>
+                                            <p>Score: {titles.score}</p>
+                                            <p>Rank: {titles.rank}</p>
+                                            <div className="slide-genres">
+                                                {titles.genres.map((genre: any) => <p className="slide-name" key={genre.mal_id}>{genre.name}</p>
+                                                )}
+                                            </div>
                                         </div>
+                                        <p className="slider-synopsis">
+                                            {titles.synopsis}
+                                        </p>
                                     </div>
-                                    <p className="slider-synopsis">
-                                        {titles.synopsis}
-                                    </p>
                                 </div>
                             </div>
                         </div>
-                    )}
-                </div>
-            </div>
-            <div className="slide-show-dots">
-                {randomAnimes && randomAnimes.map((_: string, idx: number) => (
-                    <div
-                        key={idx}
-                        className={`slide-show-dot${index === idx ? " active" : ""}`}
-                        onClick={() => {setIndex(idx)}}
-                    >
+                        )}
                     </div>
-                ))}
-            </div>
-        </div>
+                </div>
+                <div className="slide-show-dots">
+                        {randomAnimes && randomAnimes.map((_: string, idx: number) => (
+                            <div
+                                key={idx}
+                                className={`slide-show-dot${index === idx ? " active" : ""}`}
+                                onClick={() => { setIndex(idx); } }
+                            >
+                            </div>
+                        ))}
+                </div>
+        </div> : <Loading/>
     )
 }
