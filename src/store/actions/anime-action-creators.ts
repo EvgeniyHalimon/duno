62a5f5e9f20acb444c384c693b41d55ac611c69f
EvgeniyHalimon <halimonevgeniy@gmail.com>
2022-01-23 20:03:38 +0200

    added fetch util

--- src/store/actions/anime-action-creators.ts
+++ src/store/actions/anime-action-creators.ts
@@ -1,7 +1,6 @@
 import { Dispatch } from "redux"
 import { AnimeActionTypes } from "../action-types/anime-action-types"
-import axios  from 'axios';
-import { URL_ANIME_SEARCH, URL_RANDOM_ANIME, URL_TOP_ANIME } from "../../constants/constants";
+import { fetchAnimeData } from "../../utils/fetch";
 
 export const setAnimes = (data: any) => {
     return{
@@ -34,7 +33,7 @@ export const setLastPage = (number: number) => {
 export const fetchAnimes = () => {
     return async(dispatch: Dispatch) => {
         try {
-            const animes = await axios.get(URL_ANIME_SEARCH)
+            const animes = await fetchAnimeData.fetchAnimes()
             dispatch(setAnimes(animes.data))
         } catch (error) {
             dispatch(setAnimeError(true))
@@ -45,9 +44,7 @@ export const fetchAnimes = () => {
 export const fetchPaginatedAnimes = (page: number) => {
     return async(dispatch: Dispatch) => {
         try {
-            const animes = await axios.get(`${URL_TOP_ANIME}?page=${page}&limit=5&sort=asc`)
-            console.log("🚀 ~ file: anime-action-creators.ts ~ line 42 ~ returnasync ~ animes", animes)
-            console.log("🚀", animes.data.pagination.last_visible_page)
+            const animes = await fetchAnimeData.fetchPaginatedAnimes(page)
             dispatch(setLastPage(animes.data.pagination.last_visible_page))
             dispatch(setPaginatedAnimes(animes.data.data))
         } catch (error) {
@@ -66,11 +63,7 @@ export const setAnimeError = (bool: boolean) => {
 export const fetchRandomAnime = () => {
     return async (dispatch: Dispatch) => {
         try {
-            const randomTitle1 = await axios.get(URL_RANDOM_ANIME)
-            const randomTitle2 = await axios.get(URL_RANDOM_ANIME)
-            const randomTitle3 = await axios.get(URL_RANDOM_ANIME)
-            const randomAnimes = [randomTitle1.data.data, randomTitle2.data.data, randomTitle3.data.data]
-            console.log(randomAnimes);
+            const randomAnimes = await fetchAnimeData.fetchRandomAnime()
             dispatch(setRandomAnimes(randomAnimes))
         } catch (error) {
             dispatch(setAnimeError(true))
