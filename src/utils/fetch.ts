3d67c099b8abc7b693914b4a4304327f37a3f35b
EvgeniyHalimon <halimonevgeniy@gmail.com>
2022-07-19 12:47:49 +0300

    dev/refactoring: added axiosGet

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,87 +1,87 @@
-import axios from "axios"
 import { URL_ANIME_GENRES, URL_ANIME_SEARCH, URL_MANGA_GENRES, URL_MANGA_SEARCH, URL_RANDOM_ANIME, URL_RANDOM_MANGA, URL_TOP_ANIME, URL_TOP_MANGA } from "../constants/constants"
+import { axiosGet } from "./axiosGet"
 
 
 export const fetchAnimeData = {
     async fetchAnimes(){
-        const animes = await axios.get(URL_ANIME_SEARCH)
+        const animes = await axiosGet(URL_ANIME_SEARCH)
         return animes
     },
     async fetchPaginatedAnimes(page: number){
-        const animes = await axios.get(`${URL_TOP_ANIME}?page=${page}&limit=5`)
+        const animes = await axiosGet(`${URL_TOP_ANIME}?page=${page}&limit=5`)
         return animes
     },
     async fetchRandomAnime(){
-        const randomTitle1 = await axios.get(URL_RANDOM_ANIME)
-        const randomTitle2 = await axios.get(URL_RANDOM_ANIME)
-        const randomTitle3 = await axios.get(URL_RANDOM_ANIME)
+        const randomTitle1 = await axiosGet(URL_RANDOM_ANIME)
+        const randomTitle2 = await axiosGet(URL_RANDOM_ANIME)
+        const randomTitle3 = await axiosGet(URL_RANDOM_ANIME)
         const randomAnimes = [randomTitle1.data.data, randomTitle2.data.data, randomTitle3.data.data]
         return randomAnimes
     },
     async fetchAnimeSearch(inputValue: string | null, page: number){
-        const animes = await axios.get(`${URL_ANIME_SEARCH}?q=${inputValue}&page=${page}&limit=5&order_by=score&sort=desc`)
+        const animes = await axiosGet(`${URL_ANIME_SEARCH}?q=${inputValue}&page=${page}&limit=5&order_by=score&sort=desc`)
         return animes
     },
     async fetchAnimeGenres(){
-        const animeGenres = await axios.get(URL_ANIME_GENRES)
+        const animeGenres = await axiosGet(URL_ANIME_GENRES)
         return animeGenres
     },
     async fetchAnimesByGenres(genre: string | undefined, page: number){
-        const animesByGenres = await axios.get(`${URL_ANIME_SEARCH}?genres=${genre}&page=${page}&limit=5&sfw=false&order_by=score&sort=desc`)
+        const animesByGenres = await axiosGet(`${URL_ANIME_SEARCH}?genres=${genre}&page=${page}&limit=5&sfw=false&order_by=score&sort=desc`)
         return animesByGenres
     },
     async fetchCurrentAnimeTitle(id: string | undefined){
-        const currentAnimeTitle = await axios.get(`${URL_ANIME_SEARCH}/${id}`)
+        const currentAnimeTitle = await axiosGet(`${URL_ANIME_SEARCH}/${id}`)
         return currentAnimeTitle
     },
     async fetchPopularAnime(page: number){
-        const animes = await axios.get(`${URL_TOP_ANIME}?page=${page}&limit=10`)
+        const animes = await axiosGet(`${URL_TOP_ANIME}?page=${page}&limit=10`)
         return animes
     },
     async fetchAnimeReviews(id: number | undefined){
-        const animeReviews = await axios.get(`${URL_ANIME_SEARCH}/${id}/reviews`)
+        const animeReviews = await axiosGet(`${URL_ANIME_SEARCH}/${id}/reviews`)
         return animeReviews
     }
 }
 
 export const fetchMangaData = {
     async fetchManga(){
-        const mangas = await axios.get(URL_MANGA_SEARCH)
+        const mangas = await axiosGet(URL_MANGA_SEARCH)
         return mangas
     },
     async fetchPaginatedManga(page: number){
-        const mangas = await axios.get(`${URL_TOP_MANGA}?page=${page}&limit=5`)
+        const mangas = await axiosGet(`${URL_TOP_MANGA}?page=${page}&limit=5`)
         return mangas
     },
     async fetchRandomManga(){
-        const randomTitle1 = await axios.get(URL_RANDOM_MANGA)
-        const randomTitle2 = await axios.get(URL_RANDOM_MANGA)
-        const randomTitle3 = await axios.get(URL_RANDOM_MANGA)
+        const randomTitle1 = await axiosGet(URL_RANDOM_MANGA)
+        const randomTitle2 = await axiosGet(URL_RANDOM_MANGA)
+        const randomTitle3 = await axiosGet(URL_RANDOM_MANGA)
         const randomMangas = [randomTitle1.data.data, randomTitle2.data.data, randomTitle3.data.data]
         return randomMangas
     },
     async fetchMangaSearch(inputValue: string | null, page: number){
-        const mangas = await axios.get(`${URL_MANGA_SEARCH}?q=${inputValue}&page=${page}&limit=5&order_by=score&sort=desc`)
+        const mangas = await axiosGet(`${URL_MANGA_SEARCH}?q=${inputValue}&page=${page}&limit=5&order_by=score&sort=desc`)
         return mangas
     },
     async fetchMangaGenres(){
-        const mangaGenres = await axios.get(URL_MANGA_GENRES)
+        const mangaGenres = await axiosGet(URL_MANGA_GENRES)
         return mangaGenres
     },
     async fetchMangasByGenres(genre: string | undefined, page: number){
-        const mangaByGenres = await axios.get(`${URL_MANGA_SEARCH}?genres=${genre}&page=${page}&limit=5&sfw=false&order_by=score&sort=desc`)
+        const mangaByGenres = await axiosGet(`${URL_MANGA_SEARCH}?genres=${genre}&page=${page}&limit=5&sfw=false&order_by=score&sort=desc`)
         return mangaByGenres
     },
     async fetchCurrentMangaTitle(id: string | undefined){
-        const currentMangaTitle = await axios.get(`${URL_MANGA_SEARCH}/${id}`)
+        const currentMangaTitle = await axiosGet(`${URL_MANGA_SEARCH}/${id}`)
         return currentMangaTitle
     },
     async fetchPopularManga(page: number){
-        const mangas = await axios.get(`${URL_TOP_MANGA}?page=${page}&limit=10`)
+        const mangas = await axiosGet(`${URL_TOP_MANGA}?page=${page}&limit=10`)
         return mangas
     },
     async fetchMangaReviews(id: number | undefined){
-        const mangaReviews = await axios.get(`${URL_MANGA_SEARCH}/${id}/reviews`)
+        const mangaReviews = await axiosGet(`${URL_MANGA_SEARCH}/${id}/reviews`)
         return mangaReviews
     }
 }
