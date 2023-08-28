09699e8ea8a069c13abb1ef2e15b323a4e795fdb
EvgeniyHalimon <halimonevgeniy@gmail.com>
2023-08-28 20:46:13 +0300

    remove target in title

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -12,7 +12,7 @@ export const Title: React.FC<IPaganatedTitle> = ({title}) => {
 
     const score: any = title?.score || title?.scored
     return(
-        <Link target={'_blank'} className="titles-item" to={`/title/${title?.mal_id}`} key={title?.url}>     
+        <Link className="titles-item" to={`/title/${title?.mal_id}`} key={title?.url}>     
                 <img className="title-poster" src={title?.images?.webp.image_url} alt={`${title?.title}-Poster`} />
                 <div className="title-info">
                     <p className="info">{title?.title} / {title?.title_japanese}</p> 
