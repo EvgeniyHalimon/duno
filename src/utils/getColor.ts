5208801aa1fcb29862091922b2d7f73ee0142d8e
EvgeniyHalimon <halimonevgeniy@gmail.com>
2024-11-22 11:44:18 +0200

    fix util

--- src/utils/getColor.ts
+++ src/utils/getColor.ts
@@ -1,8 +1,8 @@
 export const getScoreColor = (score: number | string | null | undefined): string => {
     if(score){
-        if(score >= 7.5) return 'green'
-        if(score >= 5 && score <= 7.5) return 'orange'
-        if(score <= 4.9) return 'red'
+        if(Number(score) >= 7.5) return 'green'
+        if(Number(score) >= 5 && Number(score) <= 7.5) return 'orange'
+        if(Number(score) <= 4.9) return 'red'
     }
     return 'white'
 }
